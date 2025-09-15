b43da43efe36cc426ce9a20903ab43f7215b98b7
MaxiCarrillo <maxi11carrillo@gmail.com>
2025-09-15 17:21:12 -0300

    fix: update error message in IsXssSafeString decorator to English for consistency

--- materiapp-backend/src/module/common/decorators/xss-safe.decorator.ts
+++ materiapp-backend/src/module/common/decorators/xss-safe.decorator.ts
@@ -4,6 +4,6 @@ import { Matches } from 'class-validator';
 export function IsXssSafeString(message?: string) {
     const regex = /^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ-]+$/;
     return applyDecorators(
-        Matches(regex, { message: message || 'El valor contiene caracteres inválidos.' })
+        Matches(regex, { message: message || 'The value contains invalid characters.' })
     );
 }

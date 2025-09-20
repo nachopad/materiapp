b66d000314b68420e903cb4e0ffa6708692e9d52
MaxiCarrillo <maxi11carrillo@gmail.com>
2025-09-19 21:52:34 -0300

    feat: add validateCareerIds method for validating career IDs with error handling

--- materiapp-backend/src/module/career/services/career.service.ts
+++ materiapp-backend/src/module/career/services/career.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
 import { Career } from "../schemas";
 import { CreateCareerDto, UpdateCareerDto, CareerResponseDTO } from "../dtos";
 import { plainToInstance } from "class-transformer";
+import th from "zod/v4/locales/th.js";
 
 @Injectable()
 export class CarreerService {
@@ -22,7 +23,7 @@ export class CarreerService {
     }
 
     async updateCareer(id: string, updateCareerDto: UpdateCareerDto): Promise<Career> {
-        const updateCareer = await this.careerModel.findByIdAndUpdate({ _id: id }, { $set: updateCareerDto }, {new: true}).exec();
+        const updateCareer = await this.careerModel.findByIdAndUpdate({ _id: id }, { $set: updateCareerDto }, { new: true }).exec();
         if (!updateCareer) throw new NotFoundException(`Canonot update career: No career found with id: ${id}`)
         return updateCareer;
     }
@@ -40,5 +41,20 @@ export class CarreerService {
     async deleteCareerByID(id: string): Promise<Career | null> {
         await this.findCareerByID(id);
         return await this.careerModel.findByIdAndDelete(id).lean();
+    }
+
+    async validateCareerIds(ids: string[]): Promise<string[]> {
+        if (!ids.length) return [];
+
+        const careersIds = Array.from(new Set(ids));
+
+        const foundCareers = await this.careerModel.find({ _id: { $in: careersIds } }).lean();
+        if (foundCareers.length !== careersIds.length) {
+            const foundIds = foundCareers.map(c => c._id.toString());
+            const missing = careersIds.filter(id => !foundIds.includes(id));
+            throw new BadRequestException(`Career IDs do not exist: ${missing.join(', ')}`);
+        }
+
+        return careersIds;
     }
 }

eb12a449d98c033bde7faab847a80702a06c4bb4
MaxiCarrillo <maxi11carrillo@gmail.com>
2025-10-23 16:51:56 -0300

    feat: add sidebar layout components including footer, menu items, and layout sidebar

---
+++ materiapp-frontend/src/shared/layout/sidebar/layout-sidebar/layout-sidebar.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router';
+
+export const LayoutSidebar = () => {
+    return (
+        <>
+            <div>layout-sidebar</div>
+            <Outlet />
+        </>
+    );
+};

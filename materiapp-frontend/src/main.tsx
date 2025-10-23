f6fbe0e9b81cd44b7a423a8d17afa5337a7a8381
MaxiCarrillo <maxi11carrillo@gmail.com>
2025-10-23 16:52:25 -0300

    feat: add react-router for improved routing and replace HomePage with Router in main.tsx

--- materiapp-frontend/src/main.tsx
+++ materiapp-frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './core/providers/themes.provider';
+import Router from './core/router';
 import './core/styles/index.css';
-import HomePage from './modules/home/home.page';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-            <HomePage />
+            <Router />
         </ThemeProvider>
     </StrictMode>
 );

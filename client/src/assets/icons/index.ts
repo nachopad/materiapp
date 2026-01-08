186b1bc0840fd3e948398449aa95222437fc8b18
MaxiCarrillo <maxi11carrillo@gmail.com>
2026-01-08 18:00:28 -0300

    feature: use svg as react components

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Google } from './google.svg?react';
+export { default as GooglePlay } from './googleplay.svg?react';
+export { default as MercadoPago } from './mercado-pago.svg?react';
+export { default as Pity } from './pity.svg?react';
